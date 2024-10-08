d7d452653e4788cbaa7423cf6119184c8e2d18aa
Julie <julieglion@gmail.com>
2024-10-08 12:51:57 +0200

    push

---
+++ animals/animals_objects.js
@@ -0,0 +1,52 @@
+"use strict";
+
+window.addEventListener("DOMContentLoaded", start);
+
+const allAnimals = [];
+
+function start() {
+  console.log("ready");
+
+  loadJSON();
+}
+
+function loadJSON() {
+  fetch("animals.json")
+    .then((response) => response.json())
+    .then((jsonData) => {
+      // when loaded, prepare objects
+      prepareObjects(jsonData);
+    });
+}
+
+function prepareObjects(jsonData) {
+  jsonData.forEach((jsonObject) => {
+    // TODO: Create new object with cleaned data - and store that in the allAnimals array
+    const data = jsonObject.fullname.split(" ");
+    allAnimals.push({ name: data[0], desc: data[2], type: data[3], age: jsonObject.age });
+  });
+
+  displayList();
+}
+
+function displayList() {
+  // clear the list
+  document.querySelector("#list tbody").innerHTML = "";
+
+  // build a new list
+  allAnimals.forEach(displayAnimal);
+}
+
+function displayAnimal(animal) {
+  // create clone
+  const clone = document.querySelector("template#animal").content.cloneNode(true);
+
+  // set clone data
+  clone.querySelector("[data-field=name]").textContent = animal.name;
+  clone.querySelector("[data-field=desc]").textContent = animal.desc;
+  clone.querySelector("[data-field=type]").textContent = animal.type;
+  clone.querySelector("[data-field=age]").textContent = animal.age;
+
+  // append clone to list
+  document.querySelector("#list tbody").appendChild(clone);
+}
